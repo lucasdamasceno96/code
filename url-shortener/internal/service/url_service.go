c6a77c2ad3cabbfb9558426905d52739f6db926c
lucasdamasceno96 <ldp21k@outlook.com>
2025-08-27 23:08:25 -0300

    feat(service) add url-service

---
+++ url-shortener/internal/service/url_service.go
@@ -0,0 +1,65 @@
+package service
+
+import (
+	"errors"
+	"time"
+
+	"github.com/lucasdamasceno96/code/url-shortener/internal/config"
+	"github.com/lucasdamasceno96/code/url-shortener/internal/domain"
+	"github.com/lucasdamasceno96/code/url-shortener/internal/repository"
+	"github.com/lucasdamasceno96/code/url-shortener/internal/util"
+)
+
+type URLService interface {
+	ShortenURL(originalURL string) (string, error)
+	Redirect(shortCode string) (string, error)
+}
+
+type urlService struct {
+	repo   repository.URLRepository
+	config config.Config
+}
+
+func NewURLService(r repository.URLRepository, c config.Config) URLService {
+	return &urlService{repo: r, config: c}
+}
+
+func (s *urlService) ShortenURL(originalURL string) (string, error) {
+	var newShortCode string
+	var exists bool
+	var err error
+
+	for {
+		newShortCode = util.GenerateShortCode(s.config.FixedLetters)
+		exists, err = s.repo.Exists(newShortCode)
+		if err != nil {
+			return "", err
+		}
+		if !exists {
+			break
+		}
+	}
+
+	shortURL := &domain.ShortURL{
+		Original:  originalURL,
+		ShortCode: newShortCode,
+		CreatedAt: time.Now().UTC().Format(time.RFC3339),
+	}
+
+	if err := s.repo.Save(shortURL); err != nil {
+		return "", err
+	}
+
+	return s.config.BaseURL + "/" + newShortCode, nil
+}
+
+func (s *urlService) Redirect(shortCode string) (string, error) {
+	shortURL, err := s.repo.FindByShortCode(shortCode)
+	if err != nil {
+		return "", err
+	}
+	if shortURL == nil {
+		return "", errors.New("short code not found")
+	}
+	return shortURL.Original, nil
+}
